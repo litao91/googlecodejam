44c8e254c6475b7adf841854866db23f49583f10
Gary LI <litao912002@gmail.com>
2013-10-11 17:42:36 +0800

    Add template

---
+++ template.py
@@ -0,0 +1,15 @@
+def read_problem(infile):
+    # do something
+def solve_problem(prob):
+
+def main():
+    infile = open(sys.argv[1], 'r')
+    outfile = open(sys.argv[2], 'w')
+    T = int(infile.next())
+    problems = [read_problem(infile) for i in range(T)]
+    results = [solve_problem(prob) for prob in problems]
+    for i in range(T):
+        outfile.write("Case #{}: {}\n".format( i+1, results[i]))
+
+if __name__ == "__main__":
+    main()
