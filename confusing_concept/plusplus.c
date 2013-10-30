30b2faf5a30babe30fcfeebfe9f85ee9b8139bb1
Gary LI <litao912002@gmail.com>
2013-10-30 16:50:23 +0800

    Added explaination

--- confusing_concept/plusplus.c
+++ confusing_concept/plusplus.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+// What the complier do
+// 1. Make a copy of x
+// 2. Do increment on the origin value
+// 3. Assign the copy back to x
 int main() {
     int x = 1;
     x = x++;
